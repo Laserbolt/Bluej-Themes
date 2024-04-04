49a74fd1fb785f1f70daedcbcc9e54ac0a995d3a
Laser bolt <160458697+Laserbolt@users.noreply.github.com>
2024-04-04 21:53:53 +0530

    Add files via upload

---
+++ Original StyleSheets/Installation.bat
@@ -0,0 +1,13 @@
+@echo off
+
+REM Define the source folder.
+set "sourcePath=C:\Users\%USERNAME%\Downloads\Bluej-Dark-Theme-main\Original StyleSheets\stylesheets"
+
+REM Define the destination folder.
+set "destinationPath=C:\Program Files\BlueJ\lib\stylesheets"
+
+REM Use xcopy to copy the folder.
+xcopy /E /I "%sourcePath%" "%destinationPath%"
+
+REM exit the batch file.
+exit
