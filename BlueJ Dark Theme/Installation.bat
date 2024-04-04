cd3497aa0eda993ec74ce462b7fc3ad1947b2e9d
Laser bolt <160458697+Laserbolt@users.noreply.github.com>
2024-04-04 22:02:34 +0530

    Add files via upload

---
+++ BlueJ Dark Theme/Installation.bat
@@ -0,0 +1,16 @@
+@echo off
+
+REM Define the source folder.
+set "sourcePath=C:\Users\%USERNAME%\Downloads\Bluej-Dark-Theme-main\BlueJ Dark Theme\stylesheets"
+
+REM Define the destination folder.
+set "destinationPath=C:\Program Files\BlueJ\lib\stylesheets"
+
+REM Use xcopy to copy the folder.
+xcopy /E /I "%sourcePath%" "%destinationPath%"
+
+REM kills bluej
+taskkill /f /im javaw.exe
+
+REM exit the batch file.
+exit
