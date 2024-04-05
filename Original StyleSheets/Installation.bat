e4547898fd0bc550193631a39ded5b81ffaf0025
Laser bolt <160458697+Laserbolt@users.noreply.github.com>
2024-04-05 19:22:54 +0530

    Update Installation.bat

--- Original StyleSheets/Installation.bat
+++ Original StyleSheets/Installation.bat
@@ -7,7 +7,7 @@ REM Define the destination folder.
 set "destinationPath=C:\Program Files\BlueJ\lib\stylesheets"
 
 REM Use xcopy to copy the folder.
-xcopy /E /I "%sourcePath%" "%destinationPath%"
+xcopy /Y /E /I "%sourcePath%" "%destinationPath%"
 
 REM kill bluej
 taskkill /f /im javaw.exe
