7956bbc97bcda3238dd0fd7281e251b4af614044
Laser bolt <160458697+Laserbolt@users.noreply.github.com>
2024-04-11 21:25:12 +0530

    Add files via upload

---
+++ Rose Theme/Installation.bat
@@ -0,0 +1,19 @@
+@echo off
+
+REM Define the source folder.
+set "sourcePath=%~dp0\stylesheets"
+
+REM Define the destination folder.
+set "destinationPath=C:\Program Files\BlueJ\lib\stylesheets"
+
+REM Use xcopy to copy the folder.
+xcopy /Y /E /I "%sourcePath%" "%destinationPath%"
+
+REM kill bluej
+taskkill /f /im javaw.exe
+
+REM starts bluej
+start /d "c:\Program Files\BlueJ" Bluej.exe
+
+REM exit the batch file.
+exit
